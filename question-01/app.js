588c32be664e7bd5a3f8903e24533f66eb49c25d
afzalahmed81 <afzalahmed123@gmail.com>
2023-04-07 16:25:36 +0500

    pushing question-01

---
+++ question-01/app.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Enter a number');
+let num2 = prompt('Enter a number');
+
+if(num1 > num2) {
+    console.log(num1, 'is the larger number')
+} else {
+    console.log(num2, 'is the larger number');
+}
