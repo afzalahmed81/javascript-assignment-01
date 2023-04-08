94990bfae50d3cbb80bccb01d77224fb1a57366b
afzalahmed81 <afzalahmed123@gmail.com>
2023-04-08 17:22:24 +0500

    updating question-01

--- question-01/app.js
+++ question-01/app.js
@@ -2,7 +2,7 @@ let num1 = Number( prompt('Enter a number'));
 let num2 = Number(prompt('Enter a number'));
 
 if(num1 > num2) {
-    console.log(um1, 'is the larger number')
+    console.log(num1, 'is the larger number')
 } else {
     console.log(num2, 'is the larger number');
 }
